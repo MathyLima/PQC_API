3297860bb2d2ff5d19f562998e303cf4d7618137
Matheus Yago Lima de Freitas <mathy.l.freitas@gmail.com>
2026-01-12 10:47:44 -0300

    feat:Entity Document

---
+++ PQC.MODULES/Auth/Domain/Entities/Document.cs
@@ -0,0 +1,21 @@
+﻿namespace PQC.MODULES.Auth.Domain.Entities
+{
+    public class Document
+    {
+        public Guid Id { get; set; }
+        public string? FileName { get; set; }
+        public string? ContentType { get; set; }
+        public long SizeInBytes { get; set; }
+        public byte[]? Content { get; set; }
+        public Guid UploadedByUserId { get; set; }
+        public DateTime UploadedAt { get; set; }
+        public bool IsActive { get; set; }
+
+        public Document()
+        {
+            Id = Guid.NewGuid();
+            UploadedAt = DateTime.UtcNow;
+            IsActive = true;
+        }
+    }
+}
