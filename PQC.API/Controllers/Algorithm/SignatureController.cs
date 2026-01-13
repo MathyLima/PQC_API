bb17f8de01b3978eb4f474faa4cc7e219dc99f39
Matheus Yago <mathy.l.freitas@gmail.com>
2026-01-13 12:51:02 -0300

    feat:controller do assinador

---
+++ PQC.API/Controllers/Algorithm/SignatureController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PQC.MODULES.Algorithm.Application.Services;
+using PQC.MODULES.Algorithm.Application.Services.UseCases;
+using System.Security.Claims;
+
+namespace PQC.API.Controllers.Algorithm
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignatureController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public SignatureController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("sign/{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> SignDocument([FromRoute] Guid Id)
+        {
+            var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userId = Guid.Parse(userIdClaim);
+
+            var algorithmConfig = _configuration.GetSection("Algorithm");
+            var executor = new AlgorithmExecutor(
+                algorithmConfig["ExecutablePath"],
+                algorithmConfig["TempDirectory"]
+            );
+
+            var useCase = new SignDocumentUseCase(executor);
+            await useCase.Execute(Id, userId);
+
+            return Ok(new { message = "Document signed successfully" });
+        }
+    }
+    
+}
